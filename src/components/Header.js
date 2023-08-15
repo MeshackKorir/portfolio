3de7dc1cd6280b313db2915c99d0d028054840f4
Meshack Korir <meshackkorir100@gmail.com>
2023-08-15 11:43:38 +0300

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Your Name</h1>
+            <p>Front-end Developer | UI/UX Enthusiast</p>
+                <nav>
+                        <a href="#projects">Projects</a>
+                        <a href="#contact">Contact</a>
+                </nav>
+        </header>
+    );
+};
+export default Header;
